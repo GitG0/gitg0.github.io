5ccc090949f20475d95e32c53437851ce986b0a0
GitG0 <51382091+GitG0@users.noreply.github.com>
2023-06-15 17:49:16 -0400

    Homepage

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,90 @@
 import HomeScroll from "../components/HomeScroll";
+import { Text, Container, Button, Overlay, createStyles, rem } from '@mantine/core';
 
-export default function Home(){
-    return(
-        <>
-            <HomeScroll></HomeScroll>
-        </>
-    );
+const useStyles = createStyles((theme) => ({
+  wrapper: {
+    position: 'relative',
+    paddingBottom: rem(130),
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+
+    [theme.fn.smallerThan('xs')]: {
+      paddingTop: rem(80),
+      paddingBottom: rem(50),
+    },
+  },
+
+  inner: {
+    position: 'relative',
+    zIndex: 1,
+  },
+
+  highlight: {
+    color: theme.colors[theme.primaryColor][4],
+  },
+
+  description: {
+    color: '#000',
+    textAlign: 'center',
+
+    [theme.fn.smallerThan('xs')]: {
+      fontSize: theme.fontSizes.md,
+      textAlign: 'left',
+    },
+  },
+
+  controls: {
+    marginTop: `calc(${theme.spacing.xl} * 1.5)`,
+    display: 'flex',
+    justifyContent: 'center',
+    paddingLeft: theme.spacing.md,
+    paddingRight: theme.spacing.md,
+
+    [theme.fn.smallerThan('xs')]: {
+      flexDirection: 'column',
+    },
+  },
+
+  control: {
+    height: rem(42),
+    fontSize: theme.fontSizes.md,
+
+    '&:not(:first-of-type)': {
+      marginLeft: theme.spacing.md,
+    },
+
+    [theme.fn.smallerThan('xs')]: {
+      '&:not(:first-of-type)': {
+        marginTop: theme.spacing.md,
+        marginLeft: 0,
+      },
+    },
+  },
+}));
+
+export default function Home() {
+  const { classes, cx } = useStyles();
+
+  return (
+    <div className={classes.wrapper}>
+      <div className={classes.inner}>
+        <HomeScroll></HomeScroll>
+
+        <Container size={640}>
+          <Text size="xl" className={classes.description}>
+            Welcome to GitG0.io! Be sure to check out my portfolio and links below 👇
+          </Text>
+        </Container>
+
+        <div className={classes.controls}>
+          <Button className={classes.control} size="lg">
+            About
+          </Button>
+          <Button className={classes.control} size="lg">
+            Portfolio
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
 }
