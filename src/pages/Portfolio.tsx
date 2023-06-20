2d687d111ce2ee29be2aa30aa94a697aa16fdc59
GitG0 <51382091+GitG0@users.noreply.github.com>
2023-06-20 16:27:00 -0400

    Portfolio change

--- src/pages/Portfolio.tsx
+++ src/pages/Portfolio.tsx
@@ -1,13 +1,13 @@
-import { Card, Image, Text, Badge, Button, Group, Container } from '@mantine/core';
+import { Text, Container } from '@mantine/core';
 
 export default function Portfolio(){
+
     return (
         <>
             {/* <div className="repo-card" data-repo="gitg0/mantine-vite"></div>
             <div className="repo-card" data-repo="DrayWilliams1/ProjectWixs"></div> */}
             <Container size={640}>
-
-
+                <Text>Portfolio</Text>
             </Container>
         </>
     )
