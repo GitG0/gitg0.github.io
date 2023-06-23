6740e22b29fa04ba08abafb92ab73f5418660c24
GitG0 <51382091+GitG0@users.noreply.github.com>
2023-06-23 16:54:38 -0400

    Styling

--- src/assets/footerData.tsx
+++ src/assets/footerData.tsx
@@ -2,62 +2,63 @@ export const footerData = [
     {
         "title": "About",
         "links": [
-          {
-            "label": "Features",
-            "link": "#"
-          },
-          {
-            "label": "Pricing",
-            "link": "#"
-          },
-          {
-            "label": "Support",
-            "link": "#"
-          },
-          {
-            "label": "Forums",
-            "link": "#"
-          }
+          // {
+          //   "label": "Features",
+          //   "link": "#"
+          // },
+          // {
+          //   "label": "Pricing",
+          //   "link": "#"
+          // },
+          // {
+          //   "label": "Support",
+          //   "link": "#"
+          // },
+          // {
+          //   "label": "Forums",
+          //   "link": "#"
+          // }
         ]
       },
       {
-        "title": "Project",
+        "title": "Portfolio",
         "links": [
-          {
-            "label": "Contribute",
-            "link": "#"
-          },
-          {
-            "label": "Media assets",
-            "link": "#"
-          },
-          {
-            "label": "Changelog",
-            "link": "#"
-          },
-          {
-            "label": "Releases",
-            "link": "#"
-          }
+          // {
+          //   "label": "Contribute",
+          //   "link": "#"
+          // },
+          // {
+          //   "label": "Media assets",
+          //   "link": "#"
+          // },
+          // {
+          //   "label": "Changelog",
+          //   "link": "#"
+          // },
+          // {
+          //   "label": "Releases",
+          //   "link": "#"
+          // }
         ]
       },
       {
-        "title": "Community",
+        "title": "Contact Me",
         "links": [
-          {
-            "label": "Join Discord",
-            "link": "#"
-          },
-          {
-            "label": "Follow on Twitter",
-            "link": "#"
-          },
-          {
-            "label": "Email newsletter",
-            "link": "#"
-          },
-          {
-            "label": "GitHub discussions",
-            "link": "#"
-          }]}
+          // {
+          //   "label": "Join Discord",
+          //   "link": "#"
+          // },
+          // {
+          //   "label": "Follow on Twitter",
+          //   "link": "#"
+          // },
+          // {
+          //   "label": "Email newsletter",
+          //   "link": "#"
+          // },
+          // {
+          //   "label": "GitHub discussions",
+          //   "link": "#"
+          // }
+        ]}
 ]
